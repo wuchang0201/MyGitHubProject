24dd05ffe292bb91facb3d0957872c3233dfa256
wuchang <18079109051@163.com>
2021-05-02 15:26:12 +0800

    commit 0502

---
+++ 文件流IO操作/readFile.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main(){
+
+	file, err := os.Open("user.txt")
+	if err != nil {
+		fmt.Println("open file failed!, err:", err)
+		return
+	}
+	defer file.Close()
+	// 使用Read方法读取数据
+	var tmp = make([]byte, 10)
+	for{
+		n, err := file.Read(tmp)
+		if err == io.EOF {
+			fmt.Println("文件读完了")
+			return
+		}
+		if err != nil {
+			fmt.Println("read file failed, err:", err)
+			return
+		}
+
+		fmt.Println(string(tmp[:n]))
+	}
+
+}
